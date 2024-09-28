c3df58167dd8e6d5aa89ec25e0a1bc7acd02b9f4
AilinNavas <navasailin18@gmai.com>
2024-09-28 16:06:05 -0300

    pequeños ajustes

--- app/ui/home/general/Footer.jsx
+++ app/ui/home/general/Footer.jsx
@@ -6,7 +6,7 @@ import email from '@/public/icons/email2.svg'
 import ig from '@/public/icons/instagram.svg'
 import ws from '@/public/icons/whatsapp.svg'
 
-const Footer = ({faqsHref, serviceHref}) => {
+const Footer = ({ faqsHref, serviceHref }) => {
     return (
         <footer className="bg-primary text-secondary py-16 ">
             <div className="max-w-7xl mx-auto px-4 ">
@@ -16,11 +16,11 @@ const Footer = ({faqsHref, serviceHref}) => {
                         <h3 className="font-arapey mb-2 text-xl lg:text-2xl">Nuestros productos</h3>
                         <hr className='border-t-2 border-secondary/50 w-[75%] mt-2 mb-4' />
                         <ul className="space-y-2 font-assistant text-lg lg:text-xl">
-                            <Link href={'/tradicional-con-riel'}><li className="mb-2 hover:text-gray ease-in-out duration-300">Cortina tradicional con riel</li></Link>
-                            <Link href={'/tradicional-con-barral'}><li className="mb-2 hover:text-gray ease-in-out duration-300">Cortina tradicional con barral</li></Link>
-                            <Link href={'/roller'}><li className="mb-2  hover:text-gray ease-in-out duration-300">Cortina roller</li></Link>
-                            <Link href={'/bandas-verticales'}><li className="mb-2 hover:text-gray ease-in-out duration-300">Bandas verticales</li></Link>
-                            <Link href={'/toldos-verticales'}><li className="mb-2 hover:text-gray ease-in-out duration-300">Toldos verticales</li></Link>
+                            <li className="mb-2 hover:text-gray ease-in-out duration-300"><Link href={'/tradicional-con-riel'}>Cortina tradicional con riel</Link></li>
+                            <li className="mb-2 hover:text-gray ease-in-out duration-300"><Link href={'/tradicional-con-barral'}>Cortina tradicional con barral</Link></li>
+                            <li className="mb-2  hover:text-gray ease-in-out duration-300"><Link href={'/roller'}>Cortina roller</Link></li>
+                            <li className="mb-2 hover:text-gray ease-in-out duration-300"><Link href={'/bandas-verticales'}>Bandas verticales</Link></li>
+                            <li className="mb-2 hover:text-gray ease-in-out duration-300"><Link href={'/toldos-verticales'}>Toldos verticales</Link></li>
                         </ul>
                     </div>
 
@@ -29,10 +29,10 @@ const Footer = ({faqsHref, serviceHref}) => {
                         <h3 className="font-arapey mb-2 text-xl lg:text-2xl">Sobre nosotros</h3>
                         <hr className='border-t-2 border-secondary/50 w-[75%] mt-2 mb-4' />
                         <ul className="space-y-2 font-assistant text-lg lg:text-xl">
-                            <Link href={'/sobre-nosotros'}><li className=" hover:text-gray ease-in-out duration-300 cursor-pointer">Empresa</li></Link>
-                            <Link href={serviceHref}><li className="mt-2 hover:text-gray ease-in-out duration-300 cursor-pointer">Servicios</li></Link>
-                            <Link href={faqsHref}><li className="mt-2 hover:text-gray ease-in-out duration-300 cursor-pointer">Preguntas frecuentes</li></Link>
-                            <Link href={'https://walink.co/82c00b'}><li className="mt-2 hover:text-gray ease-in-out duration-300 cursor-pointer">¿Necesitas ayuda?</li></Link>
+                            <li className=" hover:text-gray ease-in-out duration-300 cursor-pointer"><Link href={'/sobre-nosotros'}></Link>Empresa</li>
+                            <li className=" hover:text-gray ease-in-out duration-300 cursor-pointer"><Link href={serviceHref}>Servicios</Link></li>
+                            <li className=" hover:text-gray ease-in-out duration-300 cursor-pointer"><Link href={faqsHref}>Preguntas frecuentes</Link></li>
+                            <li className=" hover:text-gray ease-in-out duration-300 cursor-pointer"><Link href={'https://walink.co/82c00b'}>¿Necesitas ayuda?</Link></li>
                         </ul>
                     </div>
 
@@ -49,10 +49,10 @@ const Footer = ({faqsHref, serviceHref}) => {
                                 <Image src={email} alt='icono-email' className="w-5 h-5 mr-2" />
                                 ventas@vmscortinados.com
                             </li>
-                            <Link href={'https://walink.co/82c00b'}><li className="flex items-center mt-2 hover:text-gray ease-in-out duration-300 cursor-pointer">
-                         <Image src={ws} alt='icono-whatsapp' className="w-5 h-5 mr-2" />
+                            <a href={'https://walink.co/82c00b'} target='_blank'><li className="flex items-center mt-2 hover:text-gray ease-in-out duration-300 cursor-pointer">
+                                <Image src={ws} alt='icono-whatsapp' className="w-5 h-5 mr-2" />
                                 +549 351-158507607
-                            </li></Link>
+                            </li></a>
                             <a href="https://www.instagram.com/vmscortinadosyservicios/?hl=es" target='_blank'><li className="flex items-center mt-2 hover:text-gray ease-in-out duration-300 cursor-pointer">
                                 <Image src={ig} alt='icono-instagram' className="w-5 h-5 mr-2" />
                                 vmscortinados
@@ -62,10 +62,10 @@ const Footer = ({faqsHref, serviceHref}) => {
                 </div>
 
                 {/* Sección del Logo */}
-              <Link href={'/'}>
-                <div className="mt-8 md:mt-0 flex items-center justify-center">
-                    <Image src={logo} className="w-60 " />
-                </div>
+                <Link href={'/'}>
+                    <div className="mt-8 md:mt-0 flex items-center justify-center">
+                        <Image src={logo} className="w-60 " />
+                    </div>
                 </Link>
             </div>
 
