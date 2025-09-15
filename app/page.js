4c3d14f7c0796abcad4a68543c13db8b5b2afc57
AilinNavas <navasailin18@gmai.com>
2025-09-15 16:28:40 -0300

    finaliza el mantenimiento

--- app/page.js
+++ app/page.js
@@ -1,59 +1,44 @@
-// import ContactUs from "./ui/home/ContactUs";
-// import Faqs from "./ui/home/Faqs";
-// import Footer from "./ui/home/general/Footer";
-// import InfoBottom from "./ui/home/general/InfoBottom";
-// import InfoTop from "./ui/home/general/InfoTop";
-// import Navbar from "./ui/home/general/Navbar";
-// import Whatsapp from "./ui/home/general/Whatsapp";
-// import Hero from "./ui/home/Hero";
-// import OurProcess from "./ui/home/OurProcess";
-// import ProductSection from "./ui/home/ProductSection";
-// import WhyChooseUs from "./ui/home/WhyChooseUs";
-// import FaqsHome from "./ui/data/faqsHome";
-// import SliderTrust from "./ui/products/SliderTrust";
-// import NavbarMobile from "./ui/home/general/NavbarMobile";
-// import galleryData from "./ui/data/galleryData";
-// import Gallery from "./ui/products/Gallery";
+ import ContactUs from "./ui/home/ContactUs";
+ import Faqs from "./ui/home/Faqs";
+ import Footer from "./ui/home/general/Footer";
+ import InfoBottom from "./ui/home/general/InfoBottom";
+ import InfoTop from "./ui/home/general/InfoTop";
+ import Navbar from "./ui/home/general/Navbar";
+ import Whatsapp from "./ui/home/general/Whatsapp";
+ import Hero from "./ui/home/Hero";
+ import OurProcess from "./ui/home/OurProcess";
+ import ProductSection from "./ui/home/ProductSection";
+ import WhyChooseUs from "./ui/home/WhyChooseUs";
+ import FaqsHome from "./ui/data/faqsHome";
+ import SliderTrust from "./ui/products/SliderTrust";
+ import NavbarMobile from "./ui/home/general/NavbarMobile";
+ import galleryData from "./ui/data/galleryData";
+ import Gallery from "./ui/products/Gallery";
 
 
-// export default function Home() {
+ export default function Home() {
 
-//   const { title, description, images } = galleryData.sobreNosotros
-//   return (
-//     <main className="">
-//       <InfoTop faqsHref={'#faqs'} />
-//       <Navbar />
-//       <NavbarMobile />
-//       <Whatsapp />
-//       <Hero />
-//       <OurProcess />
-//       <ProductSection />
-//       <WhyChooseUs />
-//       <ContactUs />
-//       <SliderTrust />
-//       <Faqs title="Preguntas Frecuentes"
-//       description=""
-//       items={FaqsHome} id='faqs' />
-//       {/* <GetAQuote /> */}
-//       <Gallery title={title} description={description} images={images}/>
-//       <Footer faqsHref={'#faqs'} serviceHref={'#servicios'}/>
-//       <InfoBottom />
+   const { title, description, images } = galleryData.sobreNosotros
+   return (
+     <main className="">
+       <InfoTop faqsHref={'#faqs'} />
+       <Navbar />
+       <NavbarMobile />
+       <Whatsapp />
+       <Hero />
+       <OurProcess />
+       <ProductSection />
+       <WhyChooseUs />
+       <ContactUs />
+       <SliderTrust />
+       <Faqs title="Preguntas Frecuentes"
+       description=""
+       items={FaqsHome} id='faqs' />
+       {/* <GetAQuote /> */}
+       <Gallery title={title} description={description} images={images}/>
+       <Footer faqsHref={'#faqs'} serviceHref={'#servicios'}/>
+       <InfoBottom />
      
-//     </main>
-//   );
-// }
-import Image from 'next/image'
-import mantenimiento from '@/public/icons/mantenimiento.svg'
-
-export default function Home() {
-  return (
-
-    <main className="bg-primary w-[100vw] h-[100vh] flex items-center justify-center">
-    
-  
-      <Image src={mantenimiento} alt='Icono de mantenimiento' className=" mr-2" />
-      <h1 className="text-2xl md:text-5xl text-secondary">Sitio en mantenimiento</h1>
-   
-    </main>
-  )
-}
+     </main>
+   );
+ }
