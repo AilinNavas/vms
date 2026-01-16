82abe81e4a78d283c9a7365176f56486dd345dfb
AilinNavas <navasailin18@gmai.com>
2026-01-16 14:36:23 -0300

    actualizacion enlace

--- app/ui/home/general/InfoBottom.jsx
+++ app/ui/home/general/InfoBottom.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const InfoBottom = () => {
   return (
     <div className='h-6 bg-secondary text-primary font-nunito text-center flex items-center justify-center text-xs'>
-        <p>Copyright 2024 © <a href={"https://www.muchomarketingagency.com/"} target='_blank' >Creado por MUCHO<span className='font-bold'>MARKETING</span></a></p>
+        <p>Copyright 2024 © <a href={"https://www.mucho-marketing.com/"} target='_blank' >Creado por MUCHO<span className='font-bold'>MARKETING</span></a></p>
     </div>
   )
 }
